2a8fea50dc4904b964f751aaffb5b24f7b583ed3
Kia Farhang <kfarhang0@gmail.com>
2017-06-26 12:05:35 -0500

    Started pseudocode

--- src/index.js
+++ src/index.js
@@ -2,3 +2,15 @@
 'use strict';
 
 //
+
+
+
+
+
+//inspectionParser.js
+	//@param: string url
+	//Load the inspection page
+	//Parse complaint number, status, description, etc.
+	//RETURN inspection object with all relevant information
+
+//
