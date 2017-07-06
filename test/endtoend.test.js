6431d216a3514589ee6f1af3f02ec12fe9a2b469
Kia Farhang <kfarhang0@gmail.com>
2017-07-06 17:49:59 -0500

    Work on e2e tests

--- test/endtoend.test.js
+++ test/endtoend.test.js
@@ -14,16 +14,18 @@ const assert = chai.assert;
 import { grabComplaints } from '../src/complaintGrabber';
 import { parseComplaint } from '../src/complaintParser';
 import { insertComplaint, wipeTable } from '../src/database';
-import { db, pgp } from '../src/dbConnect';
+import { db } from '../src/dbConnect';
 import { createRequestObject, isComplaintObject } from '../src/utility';
 
-after(function() {
-    pgp.end();
-});
+//We don't test status, status date, frequency because those can get updated over time
 
 describe('End-to-end tests', function() {
+
     this.timeout(1000000000);
     describe('July 1-2 2017 - Four complaints', function() {
+
+        let complaints;
+
         before(async function() {
             const requestParams = await createRequestObject(new Date('7/2/17'));
 
@@ -36,20 +38,93 @@ describe('End-to-end tests', function() {
             const databaseInserts = await Promise.all(arrayOfComplaints.map((complaint) => {
                 return insertComplaint(complaint);
             }));
+
+            complaints = await Promise.all(arrayOfComplaints.map((complaint) => {
+                return db.one(`SELECT * FROM complaints WHERE tracking_number = ${complaint.trackingNumber}`);
+            }));
+
         });
 
         after(function() {
             wipeTable('complaints');
         });
 
-        it('should successfully insert complaint #262386 into the database', async function() {
+        describe('Complaints', function() {
 
-            const complaint = await db.one(`SELECT * FROM complaints WHERE tracking_number = 262386`, [true]);
+            it('should successfully insert into the database', function() {
 
-            console.log(complaint);
+                complaints.forEach((complaint) => {
+                    assert.isDefined(complaint);
+                });
+            });
 
-            assert.isTrue(true);
+            it('should have the proper tracking numbers', function() {
+
+                let expected = [262386, 262409, 262376, 262394];
+
+                complaints.forEach((complaint, index) => {
+                    assert.strictEqual(complaint.tracking_number, expected[index]);
+                });
+
+            });
+
+            it('should have the proper date received', function() {
+                let expected = [new Date('7/1/17'), new Date('7/1/17'), new Date('7/2/17'), new Date('7/2/17')];
+
+                complaints.forEach((complaint, index) => {
+                    assert.strictEqual(new Date(complaint.date_received).toString(), expected[index].toString());
+                });
+            });
+
+            it('should have the proper number complaining', function() {
+                let expected = [1, 1, 1, 1];
+
+                complaints.forEach((complaint, index) => {
+                    assert.strictEqual(complaint.number_complaining, expected[index]);
+                });
+            });
+
+
+
+            it('should have the proper nature', function() {
+                let expected = ['WATER SUPPLY QUALITY', 'USED OIL', 'TURBIDITY', 'DUST'];
+
+                complaints.forEach((complaint, index) => {
+                    assert.strictEqual(complaint.nature, expected[index]);
+                });
+            });
+
+            it('should have the proper duration', function() {
+                let expected = ['ESTIMATED', '', 'ESTIMATED', ''];
+
+                complaints.forEach((complaint, index) => {
+                    assert.strictEqual(complaint.duration, expected[index]);
+                });
+            });
+
+            it('should have the proper media', function() {
+                let expected = ['WATER', 'WASTE', 'WATER', 'AIR'];
+
+                complaints.forEach((complaint, index) => {
+                    assert.strictEqual(complaint.media, expected[index]);
+                });
+            });
+
+            it('should have the proper program', function() {
+                let expected = ['PUBLIC WATER SYSTEM/SUPPLY',
+                    'INDUSTRIAL AND HAZARDOUS WASTE - HIGH LEVEL',
+                    'WATER QUALITY - HIGH LEVEL',
+                    'AIR QUALITY - HIGH LEVEL'
+                ];
+
+                complaints.forEach((complaint, index) => {
+                    assert.strictEqual(complaint.program, expected[index]);
+                });
+            });
+
 
         });
+
+
     });
 });
