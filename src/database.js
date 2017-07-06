b20bb941928eaf4efb7db7963941062a0876ed71
Kia Farhang <kfarhang0@gmail.com>
2017-07-06 16:24:24 -0500

    Cleaned up database module

--- src/database.js
+++ src/database.js
@@ -15,16 +15,16 @@ export function insertComplaint(complaint: Object) {
 
             const values = Object.values(underscoreKeys);
 
-            return resolve(db.none({
+            db.none({
                     text: 'INSERT INTO complaints VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18)',
                     values: values
                 })
-                .then(() => {
-                    return complaint;
+                .then((result) => {
+                    resolve(complaint);
                 })
                 .catch((error) => {
-                    return error;
-                }));
+                    reject(error);
+                });
         }
     })
 }
